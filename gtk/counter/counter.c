1c4e25eba570a71f58e5bd2d9b954ced11c42c19
kaseyhinton <kaseyjameshinton@gmail.com>
2018-02-20 20:25:22 -0800

    Updates the label with the counter.

--- gtk/counter/counter.c
+++ gtk/counter/counter.c
@@ -1,12 +1,37 @@
+/* GTK3 Application That Counts To 100
+ * 
+ * 
+ * */
+
 #include <gtk/gtk.h>
+#include <string.h>
 
 static int count = 0;
-  
+
  static void increment (GtkWidget *widget,
              gpointer   data)
              {
-				 count = count + 1;
-				 g_print ("%d", count);
+				 count += 1;
+				 char text[7] = "count: ";
+				 char buffer[255];
+				 int buffer_size;
+				 
+				 buffer_size = 255;
+				 snprintf(buffer, buffer_size, "%i", count);
+
+				 strcat(text, buffer);
+				 
+				 if (count < 10)
+					text[8] = '\0';
+				else if (count < 100)
+					text[9] = '\0';
+				else if (count == 100)
+					text[10] = '\0';
+				else
+					return;
+				 
+				 GtkWidget *label = (GtkWidget *) data;
+				 gtk_label_set_text (GTK_LABEL(label), text);
 			 }
 
 
@@ -27,13 +52,17 @@ activate (GtkApplication* app,
   
   button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
   gtk_container_add (GTK_CONTAINER (window), button_box);
-  printf("%d",count);
-  label = gtk_label_new("Count: ");
+
+  char text[7] = "count: ";
+  char buffer[8];
+  snprintf(buffer, 2, "%i", count);
+  strcat(text, buffer);
+  text[8] = '\0';
+  label = gtk_label_new(text);
   gtk_container_add (GTK_CONTAINER (button_box), label);
 
-
   button = gtk_button_new_with_label ("Increment");
-  g_signal_connect (button, "clicked", G_CALLBACK (increment), NULL);
+  g_signal_connect (button, "clicked", G_CALLBACK (increment), label);
   gtk_container_add (GTK_CONTAINER (button_box), button);
 
   gtk_widget_show_all (window);
